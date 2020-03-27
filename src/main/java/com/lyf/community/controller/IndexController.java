928f18a6d74a27d045470c488b8960faca61d726
liyifei <466885667@qq.com>
2020-03-27 16:33:58 +0800

    首页设置

--- src/main/java/com/lyf/community/controller/IndexController.java
+++ src/main/java/com/lyf/community/controller/IndexController.java
@@ -6,11 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class HelloController {
+public class IndexController {
 
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name = "name") String name, Model model) {
-        model.addAttribute("name", name);
-        return "hello";
+    @GetMapping("/")
+    public String index() {
+        return "index";
     }
 }

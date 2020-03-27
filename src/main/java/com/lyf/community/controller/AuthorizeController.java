2adabacf79a03f1852a330cae38514cbfabe3f1b
liyifei <466885667@qq.com>
2020-03-27 19:55:53 +0800

    extract github config from code to properties

--- src/main/java/com/lyf/community/controller/AuthorizeController.java
+++ src/main/java/com/lyf/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import com.lyf.community.pojo.AccessTokenPOJO;
 import com.lyf.community.provider.GithubProvider;
 import com.lyf.community.provider.GithubUser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,14 +16,23 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state) {
         AccessTokenPOJO accessTokenPOJO = new AccessTokenPOJO();
-        accessTokenPOJO.setClient_id("dc1a97ba4765459476bd");
-        accessTokenPOJO.setClient_secret("d277e15b7c2460d475a2444807825dc27a679e02");
+        accessTokenPOJO.setClient_id(clientId);
+        accessTokenPOJO.setClient_secret(clientSecret);
         accessTokenPOJO.setCode(code);
-        accessTokenPOJO.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenPOJO.setRedirect_uri(redirectUri);
         accessTokenPOJO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenPOJO);
         GithubUser user = githubProvider.getUser(accessToken);

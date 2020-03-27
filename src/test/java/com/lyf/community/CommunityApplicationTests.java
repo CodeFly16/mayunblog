5c0f4ffa2de9acca3718ce55cd4966fa18cc9dd6
liyifei <466885667@qq.com>
2020-03-27 16:01:03 +0800

    init

---
+++ src/test/java/com/lyf/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyf.community;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
